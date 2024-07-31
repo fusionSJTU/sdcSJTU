a1714714f8f62465709356e8b15a255818fc7dd9
FusionSJTU <fusion@sjtu.edu.cn>
2024-07-31 15:50:09 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = '_static'
+# html_static_path = '_static'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
